f08cd5b84b2c4a0712b14bf532db6830a6d265f2
elahelahe <elaheshojaeehabibzadeh@gmail.com>
2026-02-10 09:57:25 +0100

    add pragma once

--- include/geom/Lines.h
+++ include/geom/Lines.h
@@ -1,4 +1,5 @@
 #include "point.h"
+#pragma once
 namespace geom {
 	struct Line {
 		double a{};
@@ -13,10 +14,30 @@ namespace geom {
 	struct LineSegment {
 		Point start;
 		Point end;
+		Line line;
+
+		LineSegment() : start(0, 0), end(0, 0) { updateLine(); }
+		LineSegment(const Point& s, const Point& e) : start(s), end(e) { updateLine(); }
+
+		void updateLine() {
+			// identical points -> undefined line
+			if (start.x == end.x && start.y == end.y) {
+				line.a = std::numeric_limits<double>::quiet_NaN();
+				line.b = std::numeric_limits<double>::quiet_NaN();
+				return;
+			}
+
+			// vertical line -> represent as a = +inf, b = x
+			if (start.x == end.x) {
+				line.a = std::numeric_limits<double>::infinity();
+				line.b = start.x;
+				return;
+			}
+
+			// standard slope-intercept
+			line.a = (end.y - start.y) / (end.x - start.x); // slope
+			line.b = start.y - line.a * start.x;           // intercept
+		}
 	};
 
-	struct SupportedLine {
-		Line line;
-		LineSegment segment;
-	};
 }
